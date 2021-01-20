2652b27b2312354d075c806e8625eedf3a9b0721
carey036 <15318188117@189.cn>
2021-01-20 18:16:18 +0800

    revised

--- job.py
+++ job.py
@@ -3,5 +3,7 @@ s = requests.session()
 with open("urls.txt","r") as f:
     urls = f.readlines()
     f.close()
+#print (urls)
 for url in urls:
-    print(url+ " " +str(s.get(url.replace(" ", "")).status)+"\n")
+    print(url + str(int(s.get(url.strip()).status_code))+"\n")
+    #print(url+ " " +str(s.get("http://www.baidu.com").status)+"\n")
